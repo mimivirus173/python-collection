8c26b47e8bc7711dbff831b1d6c36a45a8c4c840
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2022-09-29 18:15:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-import subprocess
-subprocess.Popen('cmd.exe')
+import numbers
+import random
 
-i = 0
+# Variables
+rand = random.randint(1,100)
+points = 0
 
-while True:
-    i += 1
-    print(i)
+# Game
+while 1==1:    
+    input = int(input("Write a number between 1 and 99: "))
+
+    if input <= 99 and input >= 0:
+        # Random number
+        roll = random.randint(1,100)
+    
+        if roll >= input:
+            print("You win!")
+        
+            print("You gain",input,"points.")
+            points += input
+
+            print("Current score:",points)
+            print("--------------------")
+            continue
+        else:
+            print("You lost!")
+        
+            points == 0
+            continue
