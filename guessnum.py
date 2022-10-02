91f57050b09d3546f0a349d1b17f4aea5b3984fd
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2022-10-02 15:32:02 +0100

    Create guessnum.py
    a guess the number game
    
    created surprisingly quickly

---
+++ guessnum.py
@@ -0,0 +1,44 @@
+import numbers
+import random
+import os
+
+# Variables
+points = 0
+
+while True:
+    # Check for valid input
+    try:
+        lim1 = int(input("Lower limit for the random number: "))
+        lim2 = int(input("Upper limit for the random number: "))
+    except ValueError:
+        print("Invalid input!")
+    else:
+        # The computer's number
+        num = random.randint(lim1, lim2)
+        
+        # Check for valid input
+        try:
+            guess = int(input("Guess the number: "))
+        except ValueError:
+            print("Invalid input!")
+        else:
+            # The game
+            if guess == num and guess >= lim1 and guess <= lim2:
+                print("You win!")
+                
+                # Points
+                print("You gain 1 point.")
+                points += 1
+                
+                print("Current score:",points)
+                print("--------------------")
+            elif guess != num:
+                os.system('cls')
+                print("You lost!")
+                
+                print("Final score:",points)
+                print("--------------------")
+                points = 0
+            else:
+                print("Invalid input!")
+            continue
