89fb13566c7c7aec79542aad4df41f596549b5e9
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2023-01-30 16:39:19 +0000

    Update stigmet.py
    glopp

--- stigmet.py
+++ stigmet.py
@@ -12,40 +12,48 @@ while True:
 
         # Calculate grades
         if 0 <= value <= 6:
+            print("")
             print("-3")
 
             print("--------------------")
         
         elif 6 < value <= 33:
+            print("")
             print("00")
             
             print("--------------------")
 
         elif 33 < value <= 39:
+            print("")
             print("02")
             
             print("--------------------")
         
         elif 39 < value <= 55:
+            print("")
             print("4")
             
             print("--------------------")
         
         elif 55 < value <= 75:
+            print("")
             print("7")
             
             print("--------------------")
         
         elif 75 < value <= 92:
+            print("")
             print("10")
             
             print("--------------------")
         
         elif 92 < value <= max:
+            print("")
             print("12")
             
             print("--------------------")
         else:
+            print("")
             print("Invalid result!")
 
             print("--------------------")
