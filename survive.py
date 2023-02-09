cf0cb53dd415fdc73368a2699eb3bbb5c5f6ef4d
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2023-02-09 11:12:04 +0000

    Create survive.py

---
+++ survive.py
@@ -0,0 +1,27 @@
+# credit to ChatGPT
+
+import time
+import threading
+
+hunger = 10
+
+def reset_hunger():
+    global hunger
+    hunger = 10
+
+def input_thread():
+    global hunger
+    while hunger > 0:
+        input_value = input()
+        if input_value == 'eat':
+            reset_hunger()
+
+print("Enter 'eat' to reset the hunger level at any time:")
+
+thread = threading.Thread(target=input_thread)
+thread.start()
+
+while hunger >= 0:
+    print("Hunger level:", hunger)
+    hunger -= 1
+    time.sleep(1)
