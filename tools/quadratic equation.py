cefbbf9d96133e30cb11de81f3590e2250629395
mimivirus173 <hap0412@outlook.com>
2025-05-03 00:51:39 +0100

    Update quadratic equation.py
    was missing parenthesis

--- tools/quadratic equation.py
+++ tools/quadratic equation.py
@@ -7,11 +7,11 @@ def polynomial(a, b, c):
     if d < 0:
         return "No real solution"
     elif d == 0:
-        x = float(-b / 2*a)
+        x = float(-b / (2*a))
         return x
     else:
-        x_1 = float((-b + math.sqrt(d)) / 2*a)
-        x_2 = float((-b - math.sqrt(d)) / 2*a)
+        x_1 = float(((-b) + math.sqrt(d)) / (2*a))
+        x_2 = float(((-b) - math.sqrt(d)) / (2*a))
         return x_1, x_2
 
 ## the program
@@ -20,14 +20,19 @@ print("Calculator for quadratic equations with form \"ax^2 + bx + c = 0\"")
 print("--------------------")
 
 while True:
-    # define the variables of the equation
-    a = float(input("a = "))
-    b = float(input("b = "))
-    c = float(input("c = "))    
+    # define the variables of the equation and check if they are numbers
+    try:
+        a = float(input("a = "))
+        b = float(input("b = "))
+        c = float(input("c = "))    
+    except ValueError:
+        print("Invalid input! 'a', 'b' and 'c' must be numbers.")
+        print("--------------------")
+        continue
     
     # check for valid quadratic equation
     if a == 0:
-        print("Invalid input!")
+        print("Invalid input! 'a' cannot be zero.")
         print("--------------------")
     else:
         # print the solution to the polynomial
