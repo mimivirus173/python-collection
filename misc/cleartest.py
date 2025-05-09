b53432c8af13f6df4d76dbeb60d4fc4e4dd50397
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2025-05-09 13:57:42 +0100

    Update cleartest.py

--- misc/cleartest.py
+++ misc/cleartest.py
@@ -5,6 +5,11 @@ def clear_console() -> None:
     os.system("cls")
 
 while True:
-    print("\na"*100)
-    if keyboard.read_key():
+    print('\na' * 100)
+    answer = input('Clear console Y/N: ')
+    if answer == 'Y' or answer == 'y':
         clear_console()
+    else:
+        exit(0)
+    #if keyboard.read_key():
+        #clear_console()
