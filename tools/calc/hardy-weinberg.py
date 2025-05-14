392725d333454669f75457f0425cfc1b0ddda58a
mimivirus173 <hap0412@outlook.com>
2025-05-14 16:37:03 +0100

    Update hardy-weinberg.py
    hetta hevði verið gott at havt 2 tímar síðani

--- tools/calc/hardy-weinberg.py
+++ tools/calc/hardy-weinberg.py
@@ -1,10 +1,10 @@
-# p² + 2pq + q² = 1
+# p² + 2pq + q2 = 1
 # p + q = 1
 
 import math
 
-def calculate_freqs(res):
-    q = float(math.sqrt(res / 100))
+def calculate_freqs(q2):
+    q = float(math.sqrt(q2 / 100))
     p = float(1 - q)
     return  f"Frequency of dominant allele: {p:.3f}\n" \
             f"Frequency of recessive allele: {q:.3f}\n" \
@@ -14,9 +14,9 @@ def calculate_freqs(res):
             f"Frequency of dominant phenotype: {((p**2)+(2*p*q)):.3f}"
 
 def main():
-    res = float(input("Frequency of recessive allele (%): "))
+    q2 = float(input("Frequency of recessive allele (%): "))
     print(' ')
-    print(calculate_freqs(res))
+    print(calculate_freqs(q2))
 
 if __name__ == '__main__':
     main()
