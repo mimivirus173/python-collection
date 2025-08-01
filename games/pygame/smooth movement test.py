d2ca72ebd61d4c9cf915dd5ffc0a43b5b6f5ab73
mimivirus173 <hap0412@outlook.com>
2025-08-01 15:56:07 +0100

    Update smooth movement test.py

--- games/pygame/smooth movement test.py
+++ games/pygame/smooth movement test.py
@@ -1,14 +1,14 @@
-import pygame
+import pygame as pg
 
-pygame.init()
+pg.init()
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # player
-player = pygame.Rect((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2), 50, 50)
+player = pg.Rect((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2), 50, 50)
 
 # player variables
 player_xvel = player_yvel = 0
@@ -26,32 +26,32 @@ def movement_phys(speed, friction):
     player_yvel += (player_down + (0 - player_up)) * speed
     player_yvel = player_yvel * friction
 
-clock = pygame.time.Clock()
+clock = pg.time.Clock()
 
 run = True
 while run:
     clock.tick(60)  # cap to 60 FPS for consistency
     screen.fill((0, 0, 0))
 
-    pygame.draw.rect(screen, (255, 255, 255), player)
+    pg.draw.rect(screen, (255, 255, 255), player)
 
-    key = pygame.key.get_pressed()
+    key = pg.key.get_pressed()
     
     # update directional inputs
-    player_left = key[pygame.K_a] or key[pygame.K_LEFT]
-    player_right = key[pygame.K_d] or key[pygame.K_RIGHT]
-    player_up = key[pygame.K_w] or key[pygame.K_UP]
-    player_down = key[pygame.K_s] or key[pygame.K_DOWN]
+    player_left = key[pg.K_a] or key[pg.K_LEFT]
+    player_right = key[pg.K_d] or key[pg.K_RIGHT]
+    player_up = key[pg.K_w] or key[pg.K_UP]
+    player_down = key[pg.K_s] or key[pg.K_DOWN]
 
     movement_phys(3, 0.9)
     player.move_ip(player_xvel, player_yvel)
 
     # event handler
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or key[pygame.K_ESCAPE] == True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT or key[pg.K_ESCAPE] == True:
             run = False
     
     # updates the screen
-    pygame.display.update()
+    pg.display.update()
 
-pygame.quit()
+pg.quit()
